b9fb3980e0aa1b1aec0ccaeaa3d38084422a98e5
mdyeasinhn <mdyeasinhn@gmail.com>
2025-04-30 02:10:55 +0600

    initial file setup and database connect done

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+
+
+const app: Application = express();
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req: Request, res: Response) => {
+    res.send({
+      message: "The server is running",
+    });
+  });
+
+
+  export default app;
