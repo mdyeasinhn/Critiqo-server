81b9e854762985b7ed18430ecf2ce6a8b9e24bb1
mdyeasinhn <mdyeasinhn@gmail.com>
2025-04-30 14:21:09 +0600

    user model schema added

---
+++ prisma/migrations/20250430074754_user_model/migration.sql
@@ -0,0 +1,22 @@
+-- CreateEnum
+CREATE TYPE "UserRole" AS ENUM ('SUPPER_ADMIN', 'ADMIN', 'USER');
+
+-- CreateEnum
+CREATE TYPE "UserStaus" AS ENUM ('ACTIVE', 'BLOCKED', 'DELETED');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "UserRole" NOT NULL,
+    "needPasswordChange" BOOLEAN NOT NULL DEFAULT true,
+    "status" "UserStaus" NOT NULL DEFAULT 'ACTIVE',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
