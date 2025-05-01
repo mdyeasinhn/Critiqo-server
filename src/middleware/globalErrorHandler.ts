21757ac030c05219ef96ca7e61094c50ffed82c0
mdyeasinhn <mdyeasinhn@gmail.com>
2025-05-01 16:07:40 +0600

    auth guard and redme.md added

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -2,8 +2,9 @@ import { ErrorRequestHandler, Request, Response, NextFunction } from "express";
 import { Prisma } from "@prisma/client";
 import { ZodError } from "zod";
 import { StatusCodes } from "http-status-codes";
-import ApiError from "../app/shared/ApiError";
+
 import config from "../app/config";
+import ApiError from "../app/error/ApiError";
 
 // Define error types
 type IGenericErrorMessage = {
