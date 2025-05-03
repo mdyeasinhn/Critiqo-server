8c11ab75e59e50d6506788b24932b8468706990e
mdyeasinhn <mdyeasinhn@gmail.com>
2025-05-03 21:27:53 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import globalErrorHandler from "./middleware/globalErrorHandler";
 
 
 const app: Application = express();
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:3000'], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
