f969cde11f92e62891f1ccb55694b609a8033a39
mdyeasinhn <mdyeasinhn@gmail.com>
2025-05-26 18:12:15 +0600

    origin update

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app: Application = express();
 
-app.use(cors({ origin: ["https://critiqo-frontend-project.vercel.app","https://critiqo-frontend.vercel.app","https://revuloop-two.vercel.app"], credentials: true }));
+app.use(cors({ origin: ["https://critiqo-frontend-project.vercel.app","https://revuloop-two.vercel.app", "https://critiqo-frontend.vercel.app"], credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
